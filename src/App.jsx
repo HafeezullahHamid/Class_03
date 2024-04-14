4653f1bda9dffcda52cc1b719bf9cc2371ed10dc
HafeezullahHamid <hafeezullahhamid32@gmail.com>
2024-04-14 09:45:57 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// import FuncAndClassComp from './components/2-funcandclasscomp/FuncAndClassComp'
+// import WhyComponent from './components/1-whycomponents/WhyComponent'
+
+// import PropsParent from "./components/3-props/PropsParent"
+// import MyStates from "./components/4-states/MyStates"
+
+// import { MyreactDsc } from "./components/5-destructuring/MyreactDsc"
+import { Event } from "./components/6-events/Event"
+function App() {
+  return (
+    <>
+
+
+     {/* <WhyComponent/> */}
+     {/* <FuncAndClassComp/> */}
+     {/* <PropsParent/>
+   
+
+
+     <MyStates/> */}
+      {/* <MyreactDsc/> */}
+      <Event/>
+
+      
+    </>
+  )
+}
+
+export default App
